b5f9ffca5e1cbe5cf45603df87b662fa463fff97
Dzhavat Ushev <dzhavatushev@gmail.com>
2021-10-07 00:25:04 +0200

    Import BrowserAnimationsModule in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { BaseButtonModule } from './button/base-button.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NoopAnimationsModule, FormsModule, BaseButtonModule],
-  providers: [],
+  imports: [BrowserAnimationsModule, FormsModule, BaseButtonModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
